bb10cdc845b77f1388c63d23f78764a9b05cd3f2
jingwenwen <1449642307@qq.com>
2018-07-18 20:36:12 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <p class="p1">12333</p>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="less">
+  #app{
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+    .p1{
+      color: red;
+    }
+  }
+</style>
